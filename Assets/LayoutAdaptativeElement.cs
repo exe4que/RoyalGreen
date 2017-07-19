ee1328d7118c67c48edc28de64f7541a1c817fa2
exe4que <exe4que@gmail.com>
2017-07-19 00:39:10 -0300

    Initial commit

---
+++ Assets/LayoutAdaptativeElement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayoutAdaptativeElement : MonoBehaviour {
+
+    private RectTransform parent;
+    private float accumulatedWidth = 0f;
+
+    void Start() {
+        parent = transform.parent.GetComponent<RectTransform>();
+        foreach (Transform t in transform.parent) {
+            if (!t.name.Equals(transform.name)) {
+                accumulatedWidth += t.GetComponent<RectTransform>().rect.width;
+            }
+        }
+        if (accumulatedWidth < parent.rect.width) {
+            RectTransform r = transform.GetComponent<RectTransform>();
+            r.sizeDelta = new Vector2(parent.rect.width - accumulatedWidth, r.sizeDelta.y);
+            Debug.Log(r.sizeDelta);
+        }
+    }
+
+
+}
---
+++ Assets/SidebarManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SidebarManager : MonoBehaviour {
+
+    private GameObject[] panels;
+    private Image background;
+    private void Awake() {
+        background = GetComponent<Image>();
+        panels = new GameObject[transform.childCount];
+        for (int i = 0; i < transform.childCount; i++) {
+            panels[i] = transform.GetChild(i).gameObject;
+        }
+    }
+
+    public void ActivatePanel(int _index) {
+        if (background.enabled) background.enabled = false;
+        for (int i = 0; i < panels.Length; i++) {
+                panels[i].SetActive(i == _index);
+        }
+    }
+}
